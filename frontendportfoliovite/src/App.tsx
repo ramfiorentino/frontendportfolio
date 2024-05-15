5dd02d9df3d7ebb89640ddc12aa507a2f297e9e7
Ram Fiorentino <mariabf.bcn@gmail.com>
2024-05-15 21:54:50 +0200

    Refactored routes

--- frontendportfoliovite/src/App.tsx
+++ frontendportfoliovite/src/App.tsx
@@ -1,6 +1,6 @@
-import {
-  BrowserRouter as Router,
+import { Router,
   Link,
+  Route,
   useNavigate,
 } from "react-router-dom";
 import { projectsData } from "./projectsData"; // Import projects data
