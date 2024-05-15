82b151fc640a1fca3780d7e3b9fd72689412808f
Ram Fiorentino <mariabf.bcn@gmail.com>
2024-05-15 22:27:50 +0200

    styled mobile footer

--- frontendportfoliovite/src/Footer.tsx
+++ frontendportfoliovite/src/Footer.tsx
@@ -1,11 +1,14 @@
 function Footer() {
   return (
     <div>
-      <div className="flex text-white bg-red-500 justify-between items-center px-8">
+      <div className="hidden md:flex text-white bg-red-500 justify-between items-center px-8">
         <p> Designed and coded by ✦</p>
         <div className="text-right text-sm">
           <p>✦ RAM FIORENTINO ✦ <i>RAMFIORENTINO@PROTON.ME</i></p>
         </div>
+      </div>
+      <div className="flex flex-col text-white bg-red-500 text-center text-xs">
+        Designed and coded by<br/> ✦ RAM FIORENTINO ✦ <br/> <i>RAMFIORENTINO@PROTON.ME</i>
       </div>
     </div>
   );
