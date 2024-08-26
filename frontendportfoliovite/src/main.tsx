7b2876a51826883b9f249646a32bf4b42eb91e25
Ram Fiorentino <mariabf.bcn@gmail.com>
2024-08-26 07:55:45 +0200

    Fixed Typescript issue at Bio.tsx

--- frontendportfoliovite/src/main.tsx
+++ frontendportfoliovite/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { projectsData } from './projectsData'; // Import projects data
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProjectPage from './ProjectPage'
 import Bio from './Bio.tsx'
@@ -13,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
       <Route path="/" element={<App />} /> {/* Home page */}
-        <Route path="/project/:projectId" element={<ProjectPage projects={projectsData} />}  /> 
+        <Route path="/project/:projectId" element={<ProjectPage/>}  /> 
         <Route path="/bio" element={<Bio />} /> {/* Bio page */}
         <Route path="/contact" element={<Contact />} /> {/* Bio page */}
         <Route path="*" element={<App />} /> {/* Catch-all route for unmatched paths */}
